b51910111a1afde830f4b9dd3658ac6577f8bbce
N&S ERP Solutions <78316472+nserpsolutions@users.noreply.github.com>
2021-09-30 00:39:32 +0200

    Update nse_sa_trial_balance.sql

--- generic/nse_sa_trial_balance.sql
+++ generic/nse_sa_trial_balance.sql
@@ -15,17 +15,17 @@ LEFT JOIN accountingperiod AP
   ON T.postingperiod = AP.id
 LEFT JOIN subsidiary S
   ON S.id = TL.subsidiary
-WHERE TL.subsidiary = '6' AND
+WHERE TL.subsidiary = '<SubsidiaryInternalID>' AND /*Internal ID of the subsidiary that the report should be generated for*/
   TAL.posting = 'T' AND
   ((A.accttype IN ('Income', 'Expense', 'OthIncome', 'OthExpense', 'COGS') AND
-    AP.enddate < TO_DATE('20210901', 'YYYYMMDD') AND
-    AP.startdate > TO_DATE('20210331', 'YYYYMMDD')
+    AP.enddate < TO_DATE('<EndDateForTTBReportMonth>', 'YYYYMMDD') AND /*20210901 for Aug 2021*/
+    AP.startdate > TO_DATE('<SubsidiaryFiscalCalendarStart>', 'YYYYMMDD') /*20210331 for April-March Fiscal Calendar 2021*/
   ) OR
   (A.accttype NOT IN ('Income', 'Expense', 'OthIncome', 'OthExpense', 'COGS') AND
-    AP.enddate < TO_DATE('20210901', 'YYYYMMDD') 
+    AP.enddate < TO_DATE('<EndDateForTTBReportMonth>', 'YYYYMMDD') /*20210901 for Aug 2021*/
   )) AND
   TL.id = TAL.transactionline AND
-  A.acctnumber <> '30000000021'
+  A.acctnumber <> '<RetainedEarningsAccountNumber>' /*Account Number of the Retained Earnings Account which is calculated separately.*/
 GROUP BY A.acctnumber,
   A.fullname,
   BUILTIN.DF(TL.department),
@@ -33,7 +33,7 @@ GROUP BY A.acctnumber,
   BUILTIN.DF(TL.location) 
 HAVING SUM(TAL.amount) <> 0
 UNION SELECT SUM(-TAL.amount) AS GLAmount,
-  '30000000021' AS AccountNumber,
+  '<RetainedEarningsAccountNumber>' AS AccountNumber, /*Account Number of the Retained Earnings Account which is calculated separately.*/
   'Retained Earnings' AS AccountName,
   BUILTIN.DF(TL.department) AS Department,
   BUILTIN.DF(TL.class) AS WebSite,
@@ -49,18 +49,18 @@ LEFT JOIN accountingperiod AP
   ON T.postingperiod = AP.id
 LEFT JOIN subsidiary S
   ON S.id = TL.subsidiary
-WHERE TL.subsidiary = '6' AND
+WHERE TL.subsidiary = '<SubsidiaryInternalID>' AND /*Internal ID of the subsidiary that the report should be generated for*/
   TAL.posting = 'T' AND
   ((A.accttype IN ('Income', 'Expense', 'OthIncome', 'OthExpense', 'COGS') AND
-    AP.enddate < TO_DATE('20210901', 'YYYYMMDD') AND
-    AP.startdate > TO_DATE('20210331', 'YYYYMMDD')
+    AP.enddate < TO_DATE('<EndDateForTTBReportMonth>', 'YYYYMMDD') AND /*20210901 for Aug 2021*/
+    AP.startdate > TO_DATE('<SubsidiaryFiscalCalendarStart>', 'YYYYMMDD') /*20210331 for April-March Fiscal Calendar 2021*/
   ) OR
   (A.accttype NOT IN ('Income', 'Expense', 'OthIncome', 'OthExpense', 'COGS') AND
-    AP.enddate < TO_DATE('20210901', 'YYYYMMDD') 
+    AP.enddate < TO_DATE('<EndDateForTTBReportMonth>', 'YYYYMMDD') /*20210901 for Aug 2021*/
   )) AND
   TL.id = TAL.transactionline AND
-  A.acctnumber <> '30000000021'
-GROUP BY '30000000021',
+  A.acctnumber <> '<RetainedEarningsAccountNumber>' /*Account Number of the Retained Earnings Account which is calculated separately.*/
+GROUP BY '<RetainedEarningsAccountNumber>', /*Account Number of the Retained Earnings Account which is calculated separately.*/
   'Retained Earnings',
   BUILTIN.DF(TL.department),
   BUILTIN.DF(TL.class),
